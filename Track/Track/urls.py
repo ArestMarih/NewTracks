4924a52513960b406810f4ed8d385d7899683ef2
ArestMarih <goxota03@gmail.com>
2024-11-22 04:44:17 +0300

    Add Done, fix button

--- Track/Track/urls.py
+++ Track/Track/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from trak.views import main, deleters, Poster
+from trak.views import main, deleters, Poster, done
 from trak import views
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', main, name='main'),
     path('deleters/<int:id>/', views.deleters),
     path('Poster/', views.Poster),
+    path('done/<int:id>/', views.done),
 ]
--- Track/trak/models.py
+++ Track/trak/models.py
@@ -4,3 +4,4 @@ class quest(models.Model):
     nameQu = models.CharField(max_length=128)
     comments = models.TextField(null=True,blank=True)
     exp = models.PositiveIntegerField(default=0)
+    done = models.BooleanField(default=False)
